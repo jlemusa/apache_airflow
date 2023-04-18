61080414c5f23a08b69438b5a5aa572e6c9d564d
JonathanLemus <jonathan.lemusarriaga@hitachivantara.com>
2023-04-18 12:46:47 -0600

    Simple SSHConnection test

--- move_file.py
+++ move_file.py
@@ -1,22 +1,8 @@
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
-from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-default_args = {
-    "start_date": datetime(2022, 1, 1)
-}
+with DAG(dag_id="my_dag", start_date=datetime(2023, 1, 1), schedule_interval="@once"):
+    run_bash_command = SSHOperator(task_id="run_bash_command", command="ls -al /Files", ssh_conn_id="SSH_to_SV02")
 
-with DAG(dag_id="move_files_on_sv02", default_args=default_args, schedule_interval="@daily"):
-    move_file = SSHOperator(
-        task_id="move_file",
-        ssh_conn_id="SSH_to_SV02",
-        command="mv /Files/* /ProcessedFiles"
-    )
-
-    finish_task = BashOperator(
-        task_id="finish_task",
-        bash_command="Done!",
-    )
-
-move_file > finish_task
+run_bash_command
