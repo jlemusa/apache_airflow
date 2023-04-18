f047c53627e8027529e130e57edaf3b7e4750cea
JonathanLemus <jonathan.lemusarriaga@hitachivantara.com>
2023-04-18 13:05:02 -0600

    Simple SSHConnection test

--- move_file.py
+++ move_file.py
@@ -4,5 +4,6 @@ from datetime import datetime
 
 with DAG(dag_id="moving_files", start_date=datetime(2023, 1, 1), schedule_interval="@once"):
     list_files_and_log = SSHOperator(task_id="list_files_and_log", command=f"ls -al /Files > /LogProcess/log-{datetime.now().strftime('%Y%m%d')}", ssh_conn_id="SSH_to_SV02")
+    move_files = SSHOperator(task_id="move_files", command="mv /Files/* /ProcessedFiles/", ssh_conn_id="SSH_to_SV02")
 
-list_files_and_log
+list_files_and_log > move_files
