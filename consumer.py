b86e835a169d691217bd1e8ec5b029054e4b44bc
JonathanLemus <jonathan.lemusarriaga@hitachivantara.com>
2023-04-18 11:23:58 -0600

    Creating the DAG hub

---
+++ consumer.py
@@ -0,0 +1,14 @@
+from airflow import DAG,Dataset
+from airflow.decorators import task
+
+from datetime import date, datetime
+
+my_file = Dataset("/tmp/my_file.txt")
+
+with DAG(dag_id="consumer", schedule=[my_file], start_date=datetime(2023,1,1), catchup=False):
+    @task
+    def read_dateset():
+        with open(my_file.uri, "r") as f:
+            print(f.read())
+
+    read_dateset()
